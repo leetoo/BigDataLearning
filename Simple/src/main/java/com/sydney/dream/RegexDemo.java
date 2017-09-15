429e3455a8ee4841860fec312aa3a2b503b13e0d
easesstone <576395455@qq.com>
2017-09-15 15:02:38 +0800

    0915

---
+++ Simple/src/main/java/com/sydney/dream/RegexDemo.java
@@ -0,0 +1,11 @@
+package com.sydney.dream;
+
+import java.util.regex.Pattern;
+
+public class RegexDemo {
+    public static void main(String[] args) {
+//        System.out.println(Pattern.matches("[-_A-Za-z0-9\\\\u4e00-\\\\u9fa5]{0,30}ni" +
+//                "ma[-_A-Za-z0-9\\\\u4e00-\\\\u9fa5]{0,30}", null));
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
